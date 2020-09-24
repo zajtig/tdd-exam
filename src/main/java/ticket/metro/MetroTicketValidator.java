1aff46b3417fcd8f2695b452cee550bb573e6047
Zajti Gábor <Gabor.Zajti@idomsoft.hu>
2020-09-24 11:57:12 +0200

    3/3. refact station checking

--- src/main/java/ticket/metro/MetroTicketValidator.java
+++ src/main/java/ticket/metro/MetroTicketValidator.java
@@ -3,12 +3,21 @@ package ticket.metro;
 import ticket.metro.exception.OppositeDirectionOnTheSameLineException;
 import ticket.metro.exception.VehicleIsNotMetroException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static ticket.metro.ValidationSequenceReader.getDirectionFromValidationSequence;
 import static ticket.metro.ValidationSequenceReader.getMetroStationFromValidationSequence;
 
 public class MetroTicketValidator {
 
     private static final String METRO_SIGNATURE = "xxx";
+
+    private static final Set<String> M3_STATIONS = new HashSet<>();
+
+    {
+        M3_STATIONS.add("06");
+    }
 
     public void validate(String passengerTicket, String inspectorState) {
         checkMetroSignature(inspectorState);
@@ -23,14 +32,25 @@ public class MetroTicketValidator {
     }
 
     private void checkDirection(String passengerTicket, String inspectorState) {
-        if (getMetroStationFromValidationSequence(passengerTicket)
-                .equals(getMetroStationFromValidationSequence(inspectorState))) {
-            if (isEven(getDirectionFromValidationSequence(passengerTicket))
-                    != isEven(getDirectionFromValidationSequence(inspectorState))
-            ) {
+        if (isOnTheSameLine(passengerTicket, inspectorState)) {
+            if (isTheSameDirection(passengerTicket, inspectorState)) {
                 throw new OppositeDirectionOnTheSameLineException();
             }
         }
+    }
+
+    private boolean isTheSameDirection(String passengerTicket, String inspectorState) {
+        return isEven(getDirectionFromValidationSequence(passengerTicket))
+                != isEven(getDirectionFromValidationSequence(inspectorState));
+    }
+
+    private boolean isOnTheSameLine(String passengerTicket, String inspectorState) {
+        return isM3Station(getMetroStationFromValidationSequence(passengerTicket))
+                == isM3Station(getMetroStationFromValidationSequence(inspectorState));
+    }
+
+    private boolean isM3Station(String station) {
+        return M3_STATIONS.contains(station);
     }
 
     private boolean isEven(int number) {
